368903022dc7ab4983639387c4a0c610dea4847a
GodofPumpkins <39287703+GodofPumpkins@users.noreply.github.com>
2019-04-03 19:27:02 +1100

    Add files via upload

--- project1.c
+++ project1.c
@@ -22,10 +22,22 @@ void rotationEncrypt(char *x, int n, int k);
 void rotationDecryptKey(char *x, int n, int k);
 
 //a function to decrypt an array without knowing the rotation amount, spell checks to find legible words
-void rotationDecryptNoKeyCheck(char *x, int n);
+//returns key value
+int rotationDecryptNoKeyCheck(char *x, int n);
 
 //a function to decrypt an array without knowing the rotation amount, assumes most common letter is e
-int rotationDecryptNoKeyStatistical(char *x, int n)
+//returns key value
+int rotationDecryptNoKeyStatistical(char *x, int n);
+
+//a function to encrypt an array with a given substitution cipher
+void subEncrypt(char *x, int n, char *k);
+
+//a function to decrypt an array with a given substitution cipher
+void subDecrypt(char *x, int n, char *k);
+
+
+//a function to find the largest element of an array
+int findLargest(int *x, int n);
 
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
@@ -38,24 +50,25 @@ Last Updated 03/04/2019
 */
 int main()
 {
-	//hard code an array of letters to test cyphers
-	int k = 97; //rotation cypher amount
-	char test[] = "Hello world, this is a test to see if you can break the code";
+	//hard code an array of letters to test ciphers
+	int k = 22; //rotation cipher amount
+	char test[] = "Fourscore and seven years ago our fathers brought forth, on this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal. Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived, and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting-place for those who here gave their lives, that that nation might live. It is altogether fitting and proper that we should do this. But, in a larger sense, we cannot dedicate, we cannot consecrate—we cannot hallow—this ground. The brave men, living and dead, who struggled here, have consecrated it far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us—that from these honored dead we take increased devotion to that cause for which they here gave the last full measure of devotion—that we here highly resolve that these dead shall not have died in vain—that this nation, under God, shall have a new birth of freedom, and that government of the people, by the people, for the people, shall not perish from the earth.";
+	char subKey[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
 	int testLength = strlen(test);
-	rotationEncrypt(test, testLength, k);
-	printf("%d rotation cypher encrypt: %s\n", k, test);
-	rotationDecryptNoKey(test, testLength, k);
-	printf("%d rotation cypher decrypt: %s\n", k, test);
+	subEncrypt(test, testLength, subKey);
+	printf("substitution cipher encrypt: %s\n\n", test);
+	subDecrypt(test, testLength, subKey);
+	printf("substitution cipher decrypt: %s\n\n", test);
 
 
 
 	return 0;
 }
 
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ENCRYPTION/DECRYPTION FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 /*
-A function that takes an array of characters from a pointer 'x' of size 'n' and encrypts it using a rotation cypher, with key 'k'
+A function that takes an array of characters from a pointer 'x' of size 'n' and encrypts it using a rotation cipher, with key 'k'
 
 Task 1
 
@@ -84,7 +97,7 @@ void rotationEncrypt(char *x, int n, int k)
 
 
 /*
-A function that takes an array of characters from a pointer 'x' of size 'n' and decrypts it using a rotation cypher, with key 'k'
+A function that takes an array of characters from a pointer 'x' of size 'n' and decrypts it using a rotation cipher, with key 'k'
 This function is very similar to rotationEncrypt(), except it takes away a value of k
 
 Task 2
@@ -133,9 +146,13 @@ void rotationDecryptKey(char *x, int n, int k)
 
 
 /*
-A function that brute forces a solution for a rotation cypher, by trying all possible combinations and checking each combination for known words
+A function that brute forces a solution for a rotation cipher, by trying all possible combinations and checking each combination for known words
 
 Returns an integer that is assumed to be the key
+
+
+
+########################################### IMPORTANT complete when file io is done, implement common word checker
 
 Task 5
 
@@ -145,14 +162,16 @@ int rotationDecryptNoKeyCheck(char *x, int n)
 {
 	int k = 0;
 	char temp[n]; // array to hold copies of x to test possible keys
-	//the following for loop cycles through each possible key, ie 'i' is the cypher key
-	for(int i; i < 26, i++)
+	//the following for loop cycles through each possible key, ie 'i' is the cipher key
+	for(int i; i < 26; i++)
 	{
-		for (int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
 		{
 			
 		}
 	}
+
+
 
 	return k;
 }
@@ -163,42 +182,143 @@ A function that assumes the most common character to be e, then determines the k
 
 Returns an integer that is assumed to be the key
 
+Decrypts the array given the calculated key
+
 Task 5
 
 Last Updated 03/04/2019
 */
 int rotationDecryptNoKeyStatistical(char *x, int n)
 {
-	char temp[26]; // array to hold copies of x to test possible keys
-	//the following for loop cycles through each possible key, ie 'i' is the cypher key
+	char temp[n]; // create a temporary array to store the values of the array x, so that x can be decrypted later
+	// initialise all the values of temp to the values of x
+	for(int i = 0; i < n; i++)
+	{
+		temp[i] = x[i];
+	}
+	int k; //integer to hold value of key
+	int largestPos; //integer to hold position of most common letter
+	int tally[26]; // array to hold copies of x to test possible keys
+	for(int i = 0; i < 26; i++)
+	{
+		tally[i] = 0;
+	}
+
+	//the following for loop cycles through each position in the array x,
 	for (int i = 0; i < n; i++)
 	{
 		//convert the ASCII value of each character to a number (a = 1, b = 2 etc) then tallies each letter
-		if(x[i] >= 65 && x[i] <= 90) // if the value of x at position i is a capital letter
+		if(temp[i] >= 65 && temp[i] <= 90) // if the value of x at position i is a capital letter
 		{
-			x[i] -= 65; //converts x to a number between 1 and 26
+			temp[i] -= 65; //converts x to a number between 0 and 25
 		}
-		else if(x[i] >= 97 && x[i] <= 122) // if the value of x at position i is a lower case letter
+		else if(temp[i] >= 97 && temp[i] <= 122) // if the value of x at position i is a lower case letter
 		{
-			x[i] -= 97;//converts x to a number between 1 and 26
+			temp[i] -= 97; //converts x to a number between 0 and 25
 		}
-	}
 
-	return k;
+		tally[temp[i]]++;
+	}
+	largestPos = findLargest(tally, 26);
+	//the rotation cipher is centred around e, the 5th letter, so the 
+	if(largestPos < 4)
+	{
+		k = 22 + largestPos;
+	}
+	else
+	{
+		k = largestPos - 4;
+	}
+	/*for(int i = 0; i < 26; i++)
+	{
+		printf("%c %d\n", tally[i] + 97, tally[i]);
+		//printf("%d\n", tally[i]);
+	}*/
+
+	rotationDecryptKey(x, n, k); //decrypt 
+
+
+	k += 1; //converts the array-friendly k value to one that corresponds to a cipher key, then returns it
+	return k; 
 }
 
 
 
 /*
-A function that takes an array of characters from a pointer 'x' of size 'n' and encrypts it using a substitution cypher, with key 'k'
+A function that takes an array of characters from a pointer 'x' of size 'n' and encrypts it using a substitution cipher, with key 'k'
+
+The substitution key should be formatted such that k is an array of 26 characters, the position corresponding to the letter that each element in x should be replaced with
+eg if the third element of k is e, then every c (3rd letter) should be replaced by e
+
+assumes the substition key is all upper case
 
 Task 3
 
 Last Updated 03/04/2019
 */
-void substitutionEncrypt(char *x, int n, char *k)
+void subEncrypt(char *x, int n, char *k)
 {
+	//convert letters of x to values from 0 to 25, then substitutes
+	//if block does capital letters, else if does lower case, other ASCII characters are unchanged
+	for(int i = 0; i < n; i++)
+	{
+		if(x[i] >= 65 && x[i] <= 90) // if the value of x at position i is a capital letter
+		{
+			x[i] -= 65;
+			x[i] = k[x[i]]; //sets the value to the substitution value
+		}
+		else if(x[i] >= 97 && x[i] <= 122) // if the value of x at position i is a lower case letter
+		{
+			x[i] -= 97;
+			x[i] = k[x[i]] + 32; //sets the value to the substitution value, correcting for all elements of k being uppercase
+		}
 
+	}
+
+}
+
+
+/*
+A function that takes an array of characters from a pointer 'x' of size 'n' and decrypts it using a substitution cipher, with key 'k'
+
+The substitution key should be formatted such that k is an array of 26 characters, the position corresponding to the letter that each element in x should be replaced with
+eg if the third element of k is e, then every c (3rd letter) should be replaced by e
+
+assumes the substition key is all upper case
+
+Task 4
+
+Last Updated 03/04/2019
+*/
+
+void subDecrypt(char *x, int n, char *k)
+{
+	char position = 0; // variable to hold values ouside of for loops
+	for(int i = 0; i < n; i++) // go through every element of the array x
+	{
+		if(x[i] >= 65 && x[i] <= 90) // if the value of x at position i is a capital letter
+		{
+			for(char j = 0; x[i] != k[j]; j++) // go through the characters in the substitution cypher until you find the one that matches the character in x
+			{
+				position = j; //store the index in 'position'
+			}
+			x[i] = position + 66; //sets the value to the letter corresponding to the substitution value
+		}
+		else if(x[i] >= 97 && x[i] <= 122) // if the value of x at position i is a lower case letter
+		{
+			x[i] -= 32; // convert lower case letters to uppercase
+			for(char j = 0; x[i] != k[j]; j++)// go through the characters in the substitution cypher until you find the one that matches the character in x
+			{
+				position = j;//store the index in 'position'
+				if(position == 0)
+				{
+					printf("%d %d  ", k[j], position);
+				}
+			}
+			x[i] = position + 98; //sets the value to the letter corresponding to the substitution value, correcting for all elements of k being uppercase
+		}
+
+	}
 }
 
 
@@ -212,3 +332,23 @@ void substitutionEncrypt(char *x, int n, char *k)
 		temp[i] = x[i];
 	}
 */
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GENERAL FUNCTIONS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*
+Basic function to find the position of the largest element of an array
+
+Last Updated 03/04/2019
+*/
+int findLargest(int *x, int n)
+{
+	int largestPos = 0;
+	for(int i = 0; i < n; i++)
+	{
+		if(x[i] > x[largestPos])
+		{
+			largestPos = i;
+		}
+	}
+	return largestPos;
+}
